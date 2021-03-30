cad5a505d4a437dc7028caee89695c14e354251e
shreelakshminarayan <72684037+shreelakshminarayan@users.noreply.github.com>
2021-03-30 18:42:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var ball1,database,position;
+
+function setup(){
+    database=firebase.database()
+    console.log(database)
+    
+    createCanvas(500,500);
+    ball1 = createSprite(250,250,10,10);
+    ball1.shapeColor = "red";
+    var ball1Position=database.ref("ball/position")
+    ball1Position.on("value",readPosition,showError)
+}
+
+function draw(){
+    background("white");
+   
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+    writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+database.ref("ball/position").set({
+    "x":position.x+x,
+    "y":position.y+y, 
+})
+   
+}
+function readPosition(data){
+    position=data.val()
+    console.log(position.x)
+    console.log(position.y)
+    
+    ball1.x=position.x
+    ball1.y=position.y
+
+    
+}
+function showError(){
+    console.log("error in writing the values")
+}
+
+
+
